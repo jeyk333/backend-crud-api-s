bcc965182085c59b1729baeebecfee52ebaa6eba
Jeyanth Kanagaraj <jeyanthkanagaraj@gmail.com>
2021-02-26 12:54:31 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv/config");
 
 const app = express();
@@ -11,6 +12,7 @@ const port = process.env.PORT || 5000;
 const communityRouter = require("./routes/community");
 
 //Middlewares
+app.use(cors());
 app.use(express.json());
 app.use("/community", communityRouter);
 
